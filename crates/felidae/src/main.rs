a3a6f8d4565df46804a6ae7b469941e5d91461a4
Conor Schaefer <conor@freedom.press>
2026-01-26 10:40:27 -0800

    feat: set log level to INFO by default
    The felidae binaries were previously using tracing-subscriber with no
    default config, so no logs were omitted by default. Here we set the
    default logging level to INFO, and support additional logging levels
    via env var.

--- crates/felidae/src/main.rs
+++ crates/felidae/src/main.rs
@@ -11,9 +11,13 @@ mod cli;
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
+    // The `EnvFilter` layer is used to filter events based on `RUST_LOG`.
+    let filter_layer: EnvFilter =
+        EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?;
     tracing_subscriber::registry()
         .with(fmt::layer())
         .with(EnvFilter::from_default_env())
+        .with(filter_layer)
         .init();
     cli::Options::parse().run().await
 }
